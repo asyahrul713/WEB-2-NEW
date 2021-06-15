d307cd9366dc3c6de8145c5ab0c67559db9dab4b
ahmad syahrul <80625089+asyahrul713@users.noreply.github.com>
2021-06-15 21:37:08 +0700

    Pertemuan 15

---
+++ Pert15_cookies2.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_COOKIE['username'])) {
+echo "<h1>Cookie 'username' ada. Isinya : " .
+$_COOKIE['username'];
+} else {
+echo "<h1>Cookie 'username' TIDAK ada.</h1>";
+}
+if(isset($_COOKIE['namalengkap'])) { 
+echo "<h1>Cookie 'namalengkap' ada. Isinya : " .
+$_COOKIE['namalengkap'];
+} else {
+echo "<h1>Cookie 'namalengkap' TIDAK ada.</h1>";
+}
+echo "<h2>Klik <a href='Pert15_cookies1.php'>di sini</a> untuk
+penciptaan cookies</h2>";
+echo "<h2>Klik <a href='Pert15_cookies3.php'>di sini</a> untuk
+penghapusan cookies</h2>";
+?>
